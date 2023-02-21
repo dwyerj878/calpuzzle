d60a8e7dfbe34a3f4270c5cb75e491a8286a6627
jcdwyer <jcdwyer@jcd-dev.net>
2023-02-21 08:15:24 -0600

    add test  for fn used

--- src/tile.rs
+++ src/tile.rs
@@ -9,9 +9,26 @@ pub struct Tile {
 }
 
 impl Tile {
+    /**
+     * used 
+     *  0 - unused
+     * -1 - reserved
+     * >0 - tile id
+     */
     pub fn used(&mut self, p: i8) {
         println!("Tile.used {}:{} {},{}", self.txt, self.used, self.x, self.y);
         self.used = p;        
     }
 
+}
+
+#[test]
+/**
+ * test used fn
+ */
+fn test_used() {
+    let mut t: Tile = Tile{x : 1, y: 1, used : 0, txt : String::from("test")};
+    assert!(t.used == 0);
+    t.used(-1);
+    assert!(t.used == -1);
 }
